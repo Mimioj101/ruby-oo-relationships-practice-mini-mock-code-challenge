7a1cc1a36b0c97f4a592bd482fdeac23150f9bf1
Mimioj101 <mimioj@gmail.com>
2020-07-20 10:13:51 -0400

    Book class and Author class

--- author.rb
+++ author.rb
@@ -1,1 +1,38 @@
+require 'pry'
 
+class Author
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select do |book|
+            book.author == self
+        end
+    end 
+    
+    def write_book(title, word_count)
+        Book.new(title, self, word_count)
+    end
+
+    def total_words
+        author_total = [] 
+        Book.all.select {|book|
+            if book.author == self
+                author_total << book.word_count
+            end
+        }
+        author_total.sum
+    end
+
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,24 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
-binding.pry
+#authors
+john_s = Author.new("John Smith")
+james_b = Author.new("James Baldwin")
+maya_a = Author.new("Maya Angelou")
+jane_a = Author.new("Jane Austen")
 
+#books
+apple = Book.new("Our Apple is Red", john_s, 450)
+beal_str = Book.new("If Beal Street Could Talk", james_b, 1200)
+caged_bird = Book.new("I Know Why The Caged Bird Sings", maya_a, 1150)
+p_p = Book.new("Pride and Prejudice", jane_a, 32000)
+banana = Book.new("Our Banana is Yellow", john_s, 2000)
+kiwi = Book.new("Kiwis are Green", john_s, 3000)
+s_s = Book.new("Sense and Sensibility", jane_a, 250000)
+
+
+
+
+binding.pry
+"something"
 
